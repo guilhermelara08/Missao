dc18ad6e0d6ad8491efe2584a0e99b1bc7cfc428
guilhermelara08 <162641846+guilhermelara08@users.noreply.github.com>
2024-10-02 19:37:57 -0300

    Update perguntas.js

--- perguntas.js/perguntas.js
+++ perguntas.js/perguntas.js
@@ -1,12 +1,12 @@
 export const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Assim que saiu da escola, você se depara com uma nova tecnologia: um chat que consegue responder a todas as dúvidas e gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
             {
                 texto: "Isso é assustador!",
                 afirmacao: [
-                    "No início ficou com medo do que essa tecnologia pode fazer.",
-                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+                    "No início, você ficou com medo do que essa tecnologia pode fazer.",
+                    "Achou assustador pensar na velocidade com que a tecnologia está avançando."
                 ],
                 proxima: 1,
             },
@@ -14,93 +14,92 @@ export const perguntas = [
                 texto: "Isso é maravilhoso!",
                 afirmacao: [
                     "Quis saber como usar IA no seu dia a dia.",
-                    "Pensou que IA pode ajudar em tarefas da sua vida."
+                    "Pensou que IA pode ajudar em várias tarefas da sua vida."
                 ],
                 proxima: 2,
             },
         ]
     },
     {
-        enunciado: "Utilizar uma IA pode ser aterrorizante mesmo, e foi pensando nisso que uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Uma professora de tecnologia decidiu fazer uma sequência de aulas sobre IA. No fim de uma aula, ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+                texto: "Utiliza uma ferramenta de busca que utiliza IA para encontrar informações relevantes para o trabalho.",
                 afirmacao: [
                     "Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                    "Percebeu que a IA pode ajudar a encontrar informações de forma rápida e direcionada.",
+                    "A IA consegue explicar termos complicados de forma simplificada, ajudando nas suas pesquisas."
                 ],
                 proxima: 3,
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
+                texto: "Escreve o trabalho com base nas conversas que teve com colegas e em suas pesquisas.",
                 afirmacao: [
-                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
-                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
-                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever.",
+                    "Achou que era mais fácil procurar por respostas utilizando meios tradicionais, mesmo que levasse mais tempo.",
+                    "Teve receio sobre quais dados pessoais a IA poderia utilizar."
                 ],
                 proxima: 4,
             },
         ]
     },
     {
-        enunciado: "Ferramentas de busca que utilizam IA podem facilitar muito nosso processo de busca por informações. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        enunciado: "Imagine que você precisa participar de um debate sobre o impacto da IA no trabalho do futuro. Qual posicionamento você toma?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                texto: "Defende que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
                 afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                    "Vem impulsionando a inovação na área de IA.",
+                    "Participa do desenvolvimento de soluções criativas e melhoria de processos."
                 ],
                 proxima: 5,
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Preocupa-se com as pessoas que perderão empregos para máquinas e defende a proteção dos trabalhadores.",
                 afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                    "Criou um grupo de estudos para discutir o uso ético da IA.",
+                    "Busca reduzir as desigualdades geradas pela automação."
                 ],
                 proxima: 6,
             },
         ]
     },
     {
-        enunciado: "Novas tecnologias trazem novos desafios. Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nesse debate, como você se posiciona?",
+        enunciado: "Após a elaboração do trabalho, a professora realizou um debate. Como você se posiciona sobre o impacto da IA no trabalho do futuro?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                texto: "Defende que a IA pode criar novas oportunidades de emprego.",
                 afirmacao: [
-                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                    "Vem impulsionando a inovação na área de IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas."
                 ],
                 proxima: 7,
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                texto: "Preocupa-se com as pessoas que perderão empregos para máquinas.",
                 afirmacao: [
-                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                    "Motivou a criação de um grupo de estudos sobre o uso ético da IA.",
+                    "Busca reduzir as desigualdades geradas pela automação."
                 ],
                 proxima: 7,
             },
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Ao final da discussão, você precisa criar uma imagem que represente o que pensa sobre IA. E agora?",
         alternativas: [
             {
                 texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
                 afirmacao: [
-                    "Notou que muitas pessoas não sabem utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
-                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                    "Decidiu compartilhar seus conhecimentos de design para iniciantes.",
+                    "Acha que os meios de desenho tradicionais são mais eficazes para a criatividade."
                 ],
             },
             {
                 texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
                 afirmacao: [
-                    "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
-                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
-                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                    "Acelerou o processo de criação de trabalhos e ensina outros a utilizarem essas ferramentas.",
+                    "Compartilha artes em redes sociais para ensinar como se comunicar através da arte."
                 ],
             },
         ]
