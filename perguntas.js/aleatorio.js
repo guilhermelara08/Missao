b06fdfa9c227ac3cc0a9a2c97ea68663fc1e0bb9
guilhermelara08 <162641846+guilhermelara08@users.noreply.github.com>
2024-11-05 22:18:28 -0300

    Update aleatorio.js

--- perguntas.js/aleatorio.js
+++ perguntas.js/aleatorio.js
@@ -1,16 +1,9 @@
 const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
 
-// Função para retornar um nome aleatório da lista
-export function aleatorio(lista) {
-    if (lista.length === 0) {
-        throw new Error("A lista não pode estar vazia.");
-    }
-    const posicao = Math.floor(Math.random() * lista.length);
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
     return lista[posicao];
 }
 
-// Função para obter um nome aleatório
-export function obterNome() {
-    return aleatorio(nomes);
-}
+export const nome = aleatorio(nomes)
 
